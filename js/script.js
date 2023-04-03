9ad8b73e8e64c30aca3e1e7744f27eb53390828d
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-04-03 17:24:29 +0000

    update/inital commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Timothy Manwell All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit3-02/sw.js", {
+    scope: "/ICS2O-Unit3-02/",
+  })
+}
+
+/**
+ * This function calculates the volume of a pyramid
+ */
+function enterClicked() {
+  //input
+  const length = parseFloat(document.getElementById("length-of-pyramid").value)
+  const width = parseFloat(document.getElementById("width-of-pyramid").value)
+  const height = parseFloat(document.getElementById("height-of-pyramid").value)
+
+  //process
+  const volume = (length * width * height) / 3
+
+  //output
+  document.getElementById("volume-of-pyramid").innerHTML =
+    "The volume of the pyramid is: " + volume.toFixed(2) + " cm³."
+}
